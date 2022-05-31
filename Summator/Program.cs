c37b8f5e8ec593e5fc24b3ff44f61c96d9952e66
Tatyana Andreeva <tatyana144@abv.bg>
2022-05-31 13:10:43 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Summator
+{
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+            long result = Summator.Sum(new long[] { 1, 2, 3, 4, 5, 6 });
+            if (result ==21)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+            long expected = Summator.Average(new long[] {5, 10, 15 });
+            if (result == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+
+        }
+}
+}
---
+++ Summator/Summator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator
+{
+    public static class Summator
+    {
+        public static long Sum(long[] arr)
+        {
+           
+           
+                long sum = 0;
+                for (long i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+
+                return sum;
+        
+        }
+
+        public static long Average(long[] arr)
+        {
+
+
+            long sum = 0;
+            for (long i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum/arr.Length;
+
+        }
+    }
+}
